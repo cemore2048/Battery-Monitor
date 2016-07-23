8656326c8b07a226b9b80e310037f2c40d034d2a
ssolorzano <santosjavier22@gmail.com>
2016-07-23 14:55:15 -0700

    Added form fields with material design

--- app/src/main/java/com/demoji/rafa/demoji/MainActivity.java
+++ app/src/main/java/com/demoji/rafa/demoji/MainActivity.java
@@ -2,6 +2,14 @@ package com.demoji.rafa.demoji;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner dynamicSpinner = (Spinner) findViewById(R.id.dynamic_spinner);
+
+        String[] incomes = new String[] { "$0-$50,000", "$50,000-$100,000", "$100,000+" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, incomes);
+
+        dynamicSpinner.setAdapter(adapter);
+
+        dynamicSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/demoji/rafa/demoji/SpinnerActivity.java
@@ -0,0 +1,16 @@
+package com.demoji.rafa.demoji;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+/**
+ * Created by ssolorzano on 7/23/16.
+ */
+public class SpinnerActivity {
+}
