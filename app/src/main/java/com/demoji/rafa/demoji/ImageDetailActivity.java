f77f76ae1baabcc5d419c3b2f71baee7215dd175
Rafa Moreno <rmoreno.cesar@gmail.com>
2016-07-24 04:46:29 -0700

    moving on from animations

--- app/src/main/java/com/demoji/rafa/demoji/ImageDetailActivity.java
+++ app/src/main/java/com/demoji/rafa/demoji/ImageDetailActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.graphics.Palette;
+import android.support.v7.widget.RecyclerView;
 import android.transition.ChangeBounds;
 import android.transition.Fade;
 import android.transition.Scene;
@@ -25,169 +26,12 @@ import butterknife.OnClick;
  */
 public class ImageDetailActivity extends Activity {
 
-    public static final String EXTRA_ALBUM_ART_RESID = "EXTRA_ALBUM_ART_RESID";
-
-    @BindView(R.id.pic)
-    ImageView pic;
-    @BindView(R.id.fab)
-    ImageButton fab;
-    @BindView(R.id.title_panel)
-    ViewGroup titlePanel;
-    @BindView(R.id.track_panel) ViewGroup trackPanel;
-    @BindView(R.id.detail_container) ViewGroup detailContainer;
-
-    private TransitionManager mTransitionManager;
-    private Scene mExpandedScene;
-    private Scene mCollapsedScene;
-    private Scene mCurrentScene;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_activity_detail);
-        ButterKnife.bind(this);
-        populate();
-        setupTransitions();
+
+        Bundle bundle = getExtra
     }
 
-    private Transition createTransition() {
-        TransitionSet set = new TransitionSet();
-        set.setOrdering(TransitionSet.ORDERING_SEQUENTIAL);
-
-        Transition tFab = new Scale();
-        tFab.setDuration(150);
-        tFab.addTarget(fab);
-
-        Transition tTitle = new Fold();
-        tTitle.setDuration(150);
-        tTitle.addTarget(titlePanel);
-
-        Transition tTrack = new Fold();
-        tTrack.setDuration(150);
-        tTrack.addTarget(trackPanel);
-
-        set.addTransition(tTrack);
-        set.addTransition(tTitle);
-        set.addTransition(tFab);
-
-        return set;
-    }
-
-    @OnClick(R.id.pic)
-    public void onAlbumArtClick(View view) {
-        Transition transition = createTransition();
-        TransitionManager.beginDelayedTransition(detailContainer, transition);
-        fab.setVisibility(View.INVISIBLE);
-        titlePanel.setVisibility(View.INVISIBLE);
-        trackPanel.setVisibility(View.INVISIBLE);
-    }
-
-    @OnClick(R.id.track_panel)
-    public void onTrackPanelClicked(View view) {
-        if (mCurrentScene == mExpandedScene) {
-            mCurrentScene = mCollapsedScene;
-        }
-        else {
-            mCurrentScene = mExpandedScene;
-        }
-        mTransitionManager.transitionTo(mCurrentScene);
-    }
-
-    private void setupTransitions() {
-        mTransitionManager = new TransitionManager();
-        ViewGroup transitionRoot = detailContainer;
-
-        // Expanded scene
-        mExpandedScene = Scene.getSceneForLayout(transitionRoot,
-                R.layout.image_activity_detail_expanded, this);
-
-        mExpandedScene.setEnterAction(new Runnable() {
-            @Override
-            public void run() {
-                ButterKnife.bind(ImageDetailActivity.this);
-                populate();
-                mCurrentScene = mExpandedScene;
-            }
-        });
-
-        TransitionSet expandTransitionSet = new TransitionSet();
-        expandTransitionSet.setOrdering(TransitionSet.ORDERING_SEQUENTIAL);
-        ChangeBounds changeBounds = new ChangeBounds();
-        changeBounds.setDuration(200);
-        expandTransitionSet.addTransition(changeBounds);
-
-        Fade fadeLyrics = new Fade();
-        fadeLyrics.addTarget(R.id.lyrics);
-        fadeLyrics.setDuration(150);
-        expandTransitionSet.addTransition(fadeLyrics);
-
-        // Collapsed scene
-        mCollapsedScene = Scene.getSceneForLayout(transitionRoot,
-                R.layout.image_activity_detail, this);
-
-        mCollapsedScene.setEnterAction(new Runnable() {
-            @Override
-            public void run() {
-                ButterKnife.bind(ImageDetailActivity.this);
-                populate();
-                mCurrentScene = mCollapsedScene;
-            }
-        });
-
-        TransitionSet collapseTransitionSet = new TransitionSet();
-        collapseTransitionSet.setOrdering(TransitionSet.ORDERING_SEQUENTIAL);
-
-        Fade fadeOutLyrics = new Fade();
-        fadeOutLyrics.addTarget(R.id.lyrics);
-        fadeOutLyrics.setDuration(150);
-        collapseTransitionSet.addTransition(fadeOutLyrics);
-
-        ChangeBounds resetBounds = new ChangeBounds();
-        resetBounds.setDuration(200);
-        collapseTransitionSet.addTransition(resetBounds);
-
-        mTransitionManager.setTransition(mExpandedScene, mCollapsedScene, collapseTransitionSet);
-        mTransitionManager.setTransition(mCollapsedScene, mExpandedScene, expandTransitionSet);
-        mCollapsedScene.enter();
-
-//        postponeEnterTransition();
-    }
-
-    private void populate() {
-        int albumArtResId = getIntent().getIntExtra(EXTRA_ALBUM_ART_RESID, android.R.drawable.ic_search_category_default);
-        pic.setImageResource(albumArtResId);
-
-        Bitmap albumBitmap = getReducedBitmap(albumArtResId);
-        colorizeFromImage(albumBitmap);
-    }
-
-    private Bitmap getReducedBitmap(int albumArtResId) {
-        // reduce image size in memory to avoid memory errors
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = false;
-        options.inSampleSize = 8;
-        return BitmapFactory.decodeResource(getResources(), albumArtResId, options);
-    }
-
-    private void colorizeFromImage(Bitmap image) {
-        Palette palette = Palette.from(image).generate();
-
-        // set panel colors
-        int defaultPanelColor = 0xFF808080;
-        int defaultFabColor = 0xFFEEEEEE;
-        titlePanel.setBackgroundColor(palette.getDarkVibrantColor(defaultPanelColor));
-        trackPanel.setBackgroundColor(palette.getLightMutedColor(defaultPanelColor));
-
-        // set fab colors
-        int[][] states = new int[][]{
-                new int[]{android.R.attr.state_enabled},
-                new int[]{android.R.attr.state_pressed}
-        };
-
-        int[] colors = new int[]{
-                palette.getVibrantColor(defaultFabColor),
-                palette.getLightVibrantColor(defaultFabColor)
-        };
-//        fab.setBackgroundTintList(new ColorStateList(states, colors));
-    }
 }
--- app/src/main/java/com/demoji/rafa/demoji/MainActivity.java
+++ app/src/main/java/com/demoji/rafa/demoji/MainActivity.java
@@ -1,10 +1,18 @@
 package com.demoji.rafa.demoji;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,6 +22,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -33,6 +43,28 @@ public class MainActivity extends AppCompatActivity {
         populate();
     }
 
+    interface OnVHClickedListener {
+        void onVHClicked(ImageViewHolder vh);
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final OnVHClickedListener mListener;
+        @BindView(R.id.pic)
+        ImageView pic;
+
+        public ImageViewHolder(View itemView, OnVHClickedListener listener) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+            pic = (ImageView) itemView.findViewById(R.id.pic);
+            mListener = listener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onVHClicked(this);
+        }
+    }
     private void populate() {
         URL url = FlickrApi.composeFlickrUrl("miscellaneous", FlickerApiKey.key, 20);
         Log.d("Flickr Url", url.toString());
@@ -57,7 +89,45 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            ImageRecyclerAdapter adapter = new ImageRecyclerAdapter(getImageIds(jsonString), MainActivity.this);
+                            RecyclerView.Adapter<ImageViewHolder> adapter = new RecyclerView.Adapter<ImageViewHolder>() {
+
+                                ArrayList<Picture> picList = getImageIds(jsonString);
+                                @Override
+                                public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                                    View itemView = LayoutInflater.from(parent.getContext())
+                                            .inflate(R.layout.pic_grid_item, parent, false);
+                                    return new ImageViewHolder(itemView, new OnVHClickedListener() {
+                                        @Override
+                                        public void onVHClicked(ImageViewHolder vh) {
+                                            int position = vh.getPosition();
+                                            String albumArtResId = picList.get(position).getId();
+                                            Intent intent = new Intent(MainActivity.this, ImageDetailActivity.class);
+                                            intent.putExtra("Pic Id", albumArtResId);
+
+                                            startActivity(intent);
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onBindViewHolder(ImageViewHolder holder, int position) {
+                                    String picUrl = "";
+                                    String farm = picList.get(position).getFarm();
+                                    String id = picList.get(position).getId();
+                                    String secret = picList.get(position).getSecret();
+                                    String server = picList.get(position).getServer();
+                                    picUrl = "https://farm" + farm +".staticflickr.com/"
+                                            + server +"/" + id + "_" + secret +"_z.jpg";
+
+                                    Log.d("Pic Url", picUrl);
+                                    Picasso.with(MainActivity.this).load(picUrl).into(holder.pic);
+                                }
+
+                                @Override
+                                public int getItemCount() {
+                                    return picList.size();
+                                }
+                            };
                             recyclerView.setAdapter(adapter);
                         }
                     });
@@ -65,34 +135,34 @@ public class MainActivity extends AppCompatActivity {
                     Log.e("Error connecting", "there was an error connecting to flickr");
                 }
             }
-        });
-    }
 
-    private ArrayList<Picture> getImageIds(String jsonString) {
-        ArrayList<Picture> idList = new ArrayList<>();
-        Log.d("JSON Response", jsonString.substring(14, jsonString.length()-1));
-        try {
-            JSONObject jsonObject = new JSONObject(jsonString.substring(14, jsonString.length()-1));
-            JSONObject photos = jsonObject.getJSONObject("photos");
-            JSONArray photoIdArray = photos.getJSONArray("photo");
+            public ArrayList<Picture> getImageIds(String jsonString) {
+                ArrayList<Picture> idList = new ArrayList<>();
+                Log.d("JSON Response", jsonString.substring(14, jsonString.length()-1));
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString.substring(14, jsonString.length()-1));
+                    JSONObject photos = jsonObject.getJSONObject("photos");
+                    JSONArray photoIdArray = photos.getJSONArray("photo");
 
-            for(int i = 0; i < photoIdArray.length(); i++) {
-                String id = photoIdArray.getJSONObject(i).getString("id");
-                String secret = photoIdArray.getJSONObject(i).getString("secret");
-                String server = photoIdArray.getJSONObject(i).getString("server");
-                String farm = photoIdArray.getJSONObject(i).getString("farm");
+                    for(int i = 0; i < photoIdArray.length(); i++) {
+                        String id = photoIdArray.getJSONObject(i).getString("id");
+                        String secret = photoIdArray.getJSONObject(i).getString("secret");
+                        String server = photoIdArray.getJSONObject(i).getString("server");
+                        String farm = photoIdArray.getJSONObject(i).getString("farm");
 
-                Picture picture = new Picture();
-                picture.setId(id);
-                picture.setSecret(secret);
-                picture.setServer(server);
-                picture.setFarm(farm);
-                idList.add(picture);
+                        Picture picture = new Picture();
+                        picture.setId(id);
+                        picture.setSecret(secret);
+                        picture.setServer(server);
+                        picture.setFarm(farm);
+                        idList.add(picture);
+                    }
+                } catch(JSONException e) {
+                    Log.d("JSON can't be parsed", e.getMessage());
+                }
+
+                return idList;
             }
-        } catch(JSONException e) {
-            Log.d("JSON can't be parsed", e.getMessage());
-        }
-
-        return idList;
+        });
     }
 }
