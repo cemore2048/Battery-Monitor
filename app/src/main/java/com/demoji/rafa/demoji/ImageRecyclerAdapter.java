b663aea683f43a6221f70f731b534ac946e3ce27
Rafa Moreno <rmoreno.cesar@gmail.com>
2016-07-24 02:07:38 -0700

    recycler view functions not called

--- app/src/main/java/com/demoji/rafa/demoji/ImageRecyclerAdapter.java
+++ app/src/main/java/com/demoji/rafa/demoji/ImageRecyclerAdapter.java
@@ -1,25 +1,37 @@
 package com.demoji.rafa.demoji;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
 /**
  * Created by rafa on 7/23/16.
  */
 public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageRecyclerAdapter.ImageViewHolder> {
-
     public class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
         ImageViewHolder(View v) {
             super(v);
-            ImageView image;
+            image = (ImageView) v.findViewById(R.id.pic);
         }
     }
 
-    public ImageRecyclerAdapter() {
+    ArrayList<Picture> picId;
+    Context context;
 
+    public ImageRecyclerAdapter(ArrayList<Picture> picId, Context context) {
+        this.context = context;
+        this.picId = picId;
+
+        Log.d("Initialized Adapter", "array adapter");
     }
 
     @Override
@@ -31,11 +43,20 @@ public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageRecyclerAdap
 
     @Override
     public void onBindViewHolder(ImageViewHolder holder, int position) {
+        String picUrl = "";
+        String farm = picId.get(position).getFarm();
+        String id = picId.get(position).getId();
+        String secret = picId.get(position).getSecret();
+        String server = picId.get(position).getServer();
+        picUrl = "https://farm" + farm +".staticflickr.com/"
+               + server +"/" + id + "/" + secret +".jpg";
 
+        Log.d("Pic Url", picUrl);
+        Picasso.with(context).load(picUrl).into(holder.image);
     }
 
     @Override
     public int getItemCount() {
-        return moviesList.size();
+        return 0;
     }
 }
