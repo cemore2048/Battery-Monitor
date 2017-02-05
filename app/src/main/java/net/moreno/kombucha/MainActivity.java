e47ef2e76c0d342c048f5cc93aa22adfa14a4ad0
rafa <rmoreno.cesar@gmail.com>
2017-02-04 18:46:26 -0600

    set recyclerview, need adapter

--- app/src/main/java/net/moreno/kombucha/MainActivity.java
+++ app/src/main/java/net/moreno/kombucha/MainActivity.java
@@ -2,8 +2,22 @@ package net.moreno.kombucha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity implements ValueEventListener {
+
+    @BindView(R.id.kombucha_list)
+    RecyclerView kombuchaList;
+
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        kombuchaList.setHasFixedSize(true);
+        kombuchaList.setLayoutManager(new LinearLayoutManager(this));
+
+        
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        // Get Post object and use the values to update the UI
+        Bottle bottle = dataSnapshot.getValue(Bottle.class);
+        // ...
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+        // Getting Post failed, log a message
+        Log.w("DB Error", "loadPost:onCancelled", databaseError.toException());
+        // ...
     }
 }
