979edce18eb19c6e0e0f135361f02178ef72a808
Rafa <rmoreno.cesar@gmail.com>
2017-08-09 23:46:18 -0500

    trying to update .gitignore, adde broadcast receiver for batter connectoin

---
+++ app/src/main/java/net/rmoreno/batterymonitor/BatteryBroadcast.java
@@ -0,0 +1,19 @@
+package net.rmoreno.batterymonitor;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BatteryBroadcast extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Action: " + intent.getAction() + "\n");
+        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
+        String log = sb.toString();
+        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
+    }
+}
