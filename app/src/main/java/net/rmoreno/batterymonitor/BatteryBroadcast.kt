f6c094ef30e206b0dc088c75290ef58d9baff295
rafa <rmoreno.cesar@gmail.com>
2017-12-31 19:59:50 -0600

    deleted the declaration of the screen_on/off broadcast receiver from manifest

--- app/src/main/java/net/rmoreno/batterymonitor/BatteryBroadcast.kt
+++ app/src/main/java/net/rmoreno/batterymonitor/BatteryBroadcast.kt
@@ -7,11 +7,15 @@ import android.widget.Toast
 
 class BatteryBroadcast : BroadcastReceiver() {
 
+    var wasScreenOn: Boolean? = null
+
     override fun onReceive(context: Context, intent: Intent) {
-        val sb = StringBuilder()
-        sb.append("Action: " + intent.action + "\n")
-        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n")
-        val log = sb.toString()
-        Toast.makeText(context, log, Toast.LENGTH_LONG).show()
+        if (intent.action == Intent.ACTION_SCREEN_OFF) {
+            // TODO: start timing here
+            wasScreenOn = false
+        } else if (intent.action == Intent.ACTION_SCREEN_ON) {
+            // TODO: end timing here
+            wasScreenOn = true
+        }
     }
 }
